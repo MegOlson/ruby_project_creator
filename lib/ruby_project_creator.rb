c31404a4e43e596a06041adfd7558218be16c905
Kristen Kulha and Megan Olson <megan@>
2017-09-07 16:02:59 -0700

    take away parenthesis

--- lib/ruby_project_creator.rb
+++ lib/ruby_project_creator.rb
@@ -9,9 +9,9 @@ class Create
     FileUtils.touch(name + "/README.md")
     FileUtils.touch(name + "/Gemfile")
     writer = File.open(name+"/Gemfile", 'w')
-    writer.puts ("source 'https://rubygems.org'")
-    writer.puts ("gem 'rspec'")
-    writer.puts ("gem 'pry'")
+    writer.puts "source 'https://rubygems.org'"
+    writer.puts "gem 'rspec'"
+    writer.puts "gem 'pry'"
     writer.close
     FileUtils.touch(name + "/lib/" + name + ".rb")
     FileUtils.touch(name + "/spec/" + name + "_spec.rb")

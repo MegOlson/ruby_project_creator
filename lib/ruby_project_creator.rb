5b2b75ce805a809d951da770dca24ae248f1ec7b
Kristen Kulha and Megan Olson <megan@>
2017-09-07 14:47:06 -0700

    add usability in terminal

--- lib/ruby_project_creator.rb
+++ lib/ruby_project_creator.rb
@@ -3,12 +3,16 @@ require 'fileutils'
 class Create
   def initialize(name)
     @name = name
-    FileUtils.mkdir("../" + name)
-    FileUtils.mkdir("../" + @name + "/lib")
-    FileUtils.mkdir("../" + @name + "/spec")
-    FileUtils.touch("../" + @name + "/README.md")
-    FileUtils.touch("../" + @name + "/Gemfile")
-    FileUtils.touch("../" + @name + "/lib/" + @name + ".rb")
-    FileUtils.touch("../" + @name + "/spec/" + @name + "_spec.rb")
+    FileUtils.mkdir(name)
+    FileUtils.mkdir(name + "/lib")
+    FileUtils.mkdir(name + "/spec")
+    FileUtils.touch(name + "/README.md")
+    FileUtils.touch(name + "/Gemfile")
+    FileUtils.touch(name + "/lib/" + name + ".rb")
+    FileUtils.touch(name + "/spec/" + name + "_spec.rb")
   end
 end
+
+puts "Enter your project name in snake-case:"
+user_input = gets.chomp
+Create.new(user_input)
